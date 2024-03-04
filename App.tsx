e6b4f00f3a0e2359100fda32c01645f01edd6e49
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-04 19:24:27 -0300

    chamando o footer

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native';
 import Header from './src/components/Header';
+import Footer from './src/components/Footer';
 import styles from "./AppStyles";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
+      <Footer />
     </View>
   );
 }
