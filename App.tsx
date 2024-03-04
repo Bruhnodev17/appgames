62a4478a28ff7cf7695b6e23d57b9025bc0b3031
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-04 14:29:35 -0300

    biuld app

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import styles from "./AppStyles";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
