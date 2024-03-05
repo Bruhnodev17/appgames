185d12ca179c73393aeda5db58e94487edf3a01b
Manoel Bruno <manoelbrunop17@gmail.com>
2024-03-05 11:29:13 -0300

    ratingScore

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,14 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Image} from "react-native";
 import { IGames } from "../../data/games";
+import styles from "./styles"
 
 interface GameCardProps{
     game: IGames
 }
 
-function  GameCard ({game}: GameCardProps){
+function GameCard ({game}: GameCardProps){
+
+    const generateStarRating = (ratingScore: number)=>{
+        const numStars = Math.floor(ratingScore)
+        return "⭐️".repeat(numStars)
+    }
     return(
-        <View>
-            <Text>Game Card</Text>
+        <View style={styles.card}>
+            <Image source={{uri: game.image}}/>
+            <Text style={styles.title}>{game.name}</Text>
+            <Text style={styles.data}>{`Plataforma: ${game.platform}`}</Text>
+            <Text style={styles.data}>{`Gênero: ${game.genre}`}</Text>
+            <Text style={styles.data}>{`Data de Publicação: ${game.releaseDate}`}</Text>
+            <Text style={styles.data}>{`Público: ${game.rating}`}</Text>
+            <Text style={styles.data}>{`Empresa: ${game.developer}`}</Text>
+            <Text style={styles.data}>{generateStarRating(game.ratingScore)}</Text>
         </View>
     )
 };
